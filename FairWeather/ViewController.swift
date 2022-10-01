4f19845037682083a7bd56de325b8562578549b5
Unknoob <gabriel.unknoob@gmail.com>
2022-09-30 23:48:54 -0300

    Initial Commit

---
+++ FairWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FairWeather
+//
+//  Created by Gabriel Beltrame Silva on 30/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
